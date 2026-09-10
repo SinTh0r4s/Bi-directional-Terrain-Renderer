ff8d2b769cc0f2d2ce068301dc74849306608987
Johann Bernhardt <johann.bernhardt@posteo.de>
2026-09-10 23:12:08 +0200

    remove unused code from lod_selector

--- src/claire/terrain/lod_selector.py
+++ src/claire/terrain/lod_selector.py
@@ -35,17 +35,9 @@ class MaxErrorLodSelector(LodSelector[LodData]):
         self.camera = camera
         self._max_error_in_px = max_error_in_px
         self._hysteresis_factor = hysteresis_factor
-        self._render_threshold, self._refine_threshold = self._get_thresholds(camera)
-
-    def _get_thresholds(self, camera: HasCameraPositionResolutionFovNearplane) -> tuple[float, float]:
-        factor_k = camera.resolution.y / (2 * math.tan(math.radians(camera.fov_deg) / 2))
-        render_threshold = self._max_error_in_px * (1 - self._hysteresis_factor) / factor_k
-        refine_threshold = self._max_error_in_px / factor_k
-        return render_threshold, refine_threshold
 
     def update_camera(self, camera: HasCameraPositionResolutionFovNearplane) -> None:
         self.camera = camera
-        self._render_threshold, refine_threshold = self._get_thresholds(camera)
 
     def should_refine(self, lod_data: LodData) -> Literal["refine", "render", "use_previous"]:
         distance = lod_data.aabb.get_shortest_distance_to(self.camera)
